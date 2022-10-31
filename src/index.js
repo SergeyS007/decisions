ca9f768f5215c14eb0ac2fb924a7a8b50bd7519e
Sergey Stepanov <101949638+SergeyS007@users.noreply.github.com>
2022-10-31 21:32:37 +0200

    eeee

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
 import './index.css';
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Counter extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>0</span>
+        <button
+          type="button"
+          onClick={evt => {
+            console.log('Increment button was clicked!', evt); // работает
+            console.log('this.props: ', this.props); // работает
+          }}
+        >
+          > Increment by {step}
+        </button>
+        <button
+          type="button"
+          onClick={evt => {
+            console.log('Decrement button was clicked!', evt); // работает
+            console.log('this.props: ', this.props); // работает
+          }}
+        >
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(<Counter step={5} />, document.getElementById('root'));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Counter step={5} />
 );
